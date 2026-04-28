094a9f4001c415c3527250d5e2463ef3261f242a
tiagocramon <tiago.cramon@gmail.com>
2026-04-27 21:27:28 -0300

    Atualizacao Header, Navbar, Home e viewport mobile

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import { prepareMobileViewportForFirstRender } from './utils/mobileViewport'
+import { reloadOnceOnMobileFirstLoad } from './utils/mobileViewport'
 import App from './App.tsx'
 
-const rootElement = document.getElementById('root')!
-const root = createRoot(rootElement)
-
-prepareMobileViewportForFirstRender()
-  .catch(() => undefined)
-  .finally(() => {
-    root.render(
-      <StrictMode>
-        <App />
-      </StrictMode>,
-    )
-  })
+if (!reloadOnceOnMobileFirstLoad()) {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+}
